156e1338597d2d37e1622719147d8efd997162f5
celliaraujo <celliaraujo@gmail.com>
2023-02-19 20:56:18 -0300

    inserindo class Livro2

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,45 @@ public class PrimeiroPrograma {
         Gato gato = new Gato("Fuinha", "Bege", 3);
         System.out.println(gato);
 
+        Livro livro = new Livro("Xumiguinhas na cozinha", 56);
+        System.out.println(livro);
+
        /* int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
